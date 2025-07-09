4c484f82fe8f000b8e9319d3c4ec058c8938a485
jin <jin.shiranai@gmail.com>
2025-07-09 10:11:42 +0900

    first steps

---
+++ 01 - FIRST STEPS/01-first-steps.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php echo 'Hello from php ^_^'; ?>
+    <h2><?php echo 'PHP integrates neatly within HTML.'; ?></h2>
+    <h1>Hey, this is my first PHP file~</h1>
+    <br>
+    <h3>Roll a d6 and get: <?php echo rand(1, 6); ?></h3>
+</body>
+
+</html>
+
+<?php
+/*
+PHP is just generating HTML, which the browser then renders.
+*/
+?>
